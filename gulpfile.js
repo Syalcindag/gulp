43cb24c32109cb4a497893c0ac5d57f36ca69b51
Syalcindag <40965702+Syalcindag@users.noreply.github.com>
2021-03-24 15:58:36 +0300

    completed js task

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,14 @@
 const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
-const minifyCSS = require('gulp-csso');
-const minifyImg = require('gulp-imagemin');
-const minifyJS = require('gulp-uglify');
+var uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const del = require('del');
+const minify = require('gulp-minify');
 
-gulp.task('js', async () => {
-   return gulp.src('./src/js/**/*.js')
-   .pipe(concat('app.min.js'))
-   .pipe(minifyJS())
-   .pipe(gulp.dest('dist/js'));
+
+gulp.task('js', async function() {
+   return gulp.src('./src/js/**/*.js', { allowEmpty: true })
+   .pipe(concat('bizim.js'))
+   .pipe(minify({noSource: true}))
+   .pipe(gulp.dest('dist/js'))
 });
